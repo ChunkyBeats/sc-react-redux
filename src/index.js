80f02d9db00f770916e46daacdcfe0e728cb8b3c
Cory Ribson <crbi2550@gmail.com>
2016-11-28 15:39:09 -0800

    Installs React, babel, and sets up index.js with React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('My SoundCloud React Redux App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My SoundCloud React Redux App';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,16 @@ module.exports = {
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: __dirname + '/dist',
     publicPath: '/',
@@ -12,5 +22,5 @@ module.exports = {
   devServer: {
     contentBase: './dist',
     hot: 'true'
-  }
+  },
 };
